ad4086809cf283c5974ea584c5d347a4c107a221
hiro1776 <hiro1776>
2020-06-04 02:33:03 +0900

    finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,27 +22,32 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to @book, notice: 'Book was successfully created.'
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book.id)
     else
-      render :index
+      @books = Book.all
+      render 'index'
     end
   end
 
   # PATCH/PUT /books/1
   # PATCH/PUT /books/1.json
   def update
-    if @book.update()
-      redirect_to @book, notice: 'Book was successfully updated.'
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book.id)
     else
-      render :edit
+      render 'edit'
     end
   end
 
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
-    @book.destroy
-    redirect_to books_url, notice: 'Book was successfully destroyed.'
+    if @book.destroy
+      flash[:notice] = 'Book was successfully destroyed.'
+    redirect_to books_path
+    end
   end
 
   private
